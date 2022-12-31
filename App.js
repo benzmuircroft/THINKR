92a9e0fbf9aaa5fd1a90660446ef2a9a93781dc4
Benz Muircroft (Am/is) <benzmuircroft@gmail.com>
2022-12-31 17:26:56 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,29 +1,58 @@
 import './App.css';
+import React, {useState,useRef,useReducer} from 'react';
 import Navbar from './components/Navbar.js';
-import {useState,useRef} from 'react';
-import Thought from './components/Thought';
-import {createContext} from 'react';
+import Thought from './components/Thought.js';
+
+
+
+
+
+const reducer=function(state,action){
+    return action;
+};
+
+if(!window.localStorage.getItem('folders')){
+    window.localStorage.setItem('folders',JSON.stringify(['Thoughts','Work','Reminders']));
+}   
+
+
+
+
+
+
+
+
 
 
 
 function App(){
-
-    const subject=useRef(window.localStorage.getItem('isSubject')||'thoughts');
-
-    const hasModal=useRef({editing:-1,openModal:false,trigger:null});//navbar adds the function that gets triggered by navbar,modal,thought
-
-    const [thought,sThought]=useState(()=>{
-        return JSON.parse(window.localStorage.getItem(subject.current)||'[]');
+    const [navEdit,setNavEdit]=useState(false);
+    const subjectRef=useRef(window.localStorage.getItem('isSubject')||'thoughts');
+    const folderRef=useRef(null);
+    const resumeRef=useRef(null);
+    const [modal,setModal]=useReducer(reducer,{
+        editing:-1,
+        open:false,
         });
-
+    const [thought,setThought]=useState(()=>{
+        return JSON.parse(window.localStorage.getItem(subjectRef.current)||'[]');
+    });
     let t=thought.map((v,i)=>{
-        return <Thought key={i} k={i} markdown={v} sThought={sThought} hasModal={hasModal} subject={subject}/>;
-        });
-
+        return (
+            <Thought 
+            key={i} 
+            k={i} 
+            markdown={v} 
+            setThought={setThought} 
+            setModal={setModal} 
+            subjectRef={subjectRef}
+            navEdit={navEdit}
+            folderRef={folderRef}
+            resumeRef={resumeRef}
+            />
+        );
+    });
     const [theme,setTheme]=useState(window.localStorage.getItem('theme')||'light');
-    
-    const ThemeContext=createContext(theme);
-    
     const toggleTheme=()=>{
         if(theme==='light'){
             setTheme('dark');
@@ -38,16 +67,24 @@ function App(){
         return;
         };
     document.getElementsByTagName('html')[0].style['color-scheme']=theme;
-
     return (
-        <ThemeContext.Provider value={{theme,toggleTheme}}>
-            <div className="App" id={theme}>
-                <Navbar theme={theme} toggleTheme={toggleTheme} subject={subject} sThought={sThought} hasModal={hasModal}/>
-                <header className="App-thoughts">
-                    {t}
-                </header>
-            </div>
-        </ThemeContext.Provider>
+        <div className="App" id={theme}>
+            <Navbar 
+            theme={theme} 
+            toggleTheme={toggleTheme} 
+            subjectRef={subjectRef} 
+            setThought={setThought} 
+            modal={modal} 
+            setModal={setModal} 
+            navEdit={navEdit} 
+            setNavEdit={setNavEdit}
+            folderRef={folderRef}
+            resumeRef={resumeRef}
+            />
+            <header className="App-thoughts">
+                {t}
+            </header>
+        </div>
         );
     }
 
